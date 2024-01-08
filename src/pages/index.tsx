78c276e22066f2f87eb3334e21fbb8605d960944
Koma Yuta <yutakoma54@gmail.com>
2024-01-08 10:43:36 +0900

    トップ画像ランダム表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,33 @@ import type { NextPage } from "next";
 import s from "./Index.module.scss";
 import Layout from "@/components/templates/layout/Layout";
 import PageTitle from "@/components/atoms/PageTilet/PageTilet";
+import { useEffect, useState } from "react";
+
+const RandomImage = () => {
+  const images = [
+    "./img/top_image01.jpg",
+    "./img/top_image02.jpg",
+    "./img/top_image03.jpg",
+  ];
+
+  // ランダムに画像を選ぶ関数
+  const getRandomImage = () => {
+    return images[Math.floor(Math.random() * images.length)];
+  };
+
+  const [imageUrl, setImageUrl] = useState(getRandomImage());
+
+  useEffect(() => {
+    setImageUrl(getRandomImage());
+  }, []);
+
+  return <img className={s.top_image} src={imageUrl} alt="top_images" />;
+};
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <img
-        src="./img/top_image01.jpg"
-        alt="top_image"
-        className={s.top_image}
-      />
+      <RandomImage />
       <div className={s.top_title}>
         <PageTitle>我々は宇宙人</PageTitle>
       </div>
